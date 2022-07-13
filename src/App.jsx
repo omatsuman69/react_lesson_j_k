4faebb1f28eadcc46c99c6bf07cac892711908de
omatsuman69 <69031106+omatsuman69@users.noreply.github.com>
2022-07-13 09:53:11 +0900

    learning style css

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,16 @@ import React from "react";
 import ReactDom from "react-dom";
 
 const App = () => {
+  const onClickButton = () => alert("こんにちは！");
+  const contentStyle = {
+    color: "blue",
+    fontSize: "18px"
+  };
   return (
     <>
-      <h1>こんにちは！</h1>
-      <p>お元気ですか？</p>
+      <h1 sytle={{ color: "red" }}>こんにちは！</h1>
+      <p sytple={contentStyle}>お元気ですか？</p>
+      <button onClick={onClickButton}>ボタン</button>
     </>
   );
 };
